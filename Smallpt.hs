8a429935ce56019ddcb17cbb0972ce22685fad0d
tatsy <tatsy.mail@gmail.com>
2015-08-13 18:33:38 +0900

    Add comments and type specifications.

--- Smallpt.hs
+++ Smallpt.hs
@@ -1,6 +1,6 @@
 -- Small path tracing with Haskell
 import System.Environment
-import System.Random
+import System.Random.Mersenne
 import Control.Monad
 import Codec.Picture
 import Data.Time
@@ -8,10 +8,17 @@ import qualified Data.Word as W
 import qualified Data.Vector.Storable as V
 
 -- Parameters
-eps = 1.0e-4 :: Double
-inf = 1.0e20 :: Double
-nc  = 1.0    :: Double
-nt  = 1.5    :: Double
+eps :: Double
+eps = 1.0e-4
+
+inf :: Double
+inf = 1.0e20
+
+nc :: Double
+nc  = 1.0
+
+nt :: Double
+nt  = 1.5
 
 -- Vec
 data Vec = Vec (Double, Double, Double) deriving (Show)
@@ -19,13 +26,18 @@ instance (Num Vec) where
     (Vec (x, y, z)) + (Vec (a, b, c)) = Vec (x + a, y + b, z + c)
     (Vec (x, y, z)) - (Vec (a, b, c)) = Vec (x - a, y - b, z - c)
     (Vec (x, y, z)) * (Vec (a, b, c)) = Vec (x * a, y * b, z * c)
-    abs (Vec (x, y, z)) = Vec (abs x, abs y, abs z)
-    signum (Vec (x, y, z)) = undefined
-    fromInteger a = Vec (fromIntegral a, fromIntegral a, fromIntegral a)
+    abs = undefined
+    signum = undefined
+    fromInteger x = Vec (dx, dx, dx) where dx = fromIntegral x
 
-x (Vec (x, y, z)) = x
-y (Vec (x, y, z)) = y
-z (Vec (x, y, z)) = z
+x :: Vec -> Double
+x (Vec (x, _, _)) = x
+
+y :: Vec -> Double
+y (Vec (_, y, _)) = y
+
+z :: Vec -> Double
+z (Vec (_, _, z)) = z
 
 mul :: Vec -> Double -> Vec
 mul (Vec (x, y, z)) s = Vec (x * s, y * s, z * s)
@@ -42,8 +54,12 @@ cross (Vec (x, y, z)) (Vec (a, b, c)) = Vec (y * c - b * z, z * a - c * x, x * b
 
 -- Ray
 data Ray = Ray (Vec, Vec) deriving (Show)
-org (Ray (org, dir)) = org
-dir (Ray (org, dir)) = dir
+
+org :: Ray -> Vec
+org (Ray (org, _)) = org
+
+dir :: Ray -> Vec
+dir (Ray (_, dir)) = dir
 
 -- Material
 data Refl = Diff
@@ -52,33 +68,44 @@ data Refl = Diff
           deriving Show
 
 -- Sphere
-data Sphere = Sphere (Double, Vec, Vec, Vec, Refl)
-    deriving (Show)
+data Sphere = Sphere (Double, Vec, Vec, Vec, Refl) deriving (Show)
 
-rad  (Sphere (rad, p, e, c, refl)) = rad
-pos  (Sphere (rad, p, e, c, refl)) = p
-emit (Sphere (rad, p, e, c, refl)) = e
-col  (Sphere (rad, p, e, c, refl)) = c
-refl (Sphere (rad, p, e, c, refl)) = refl
+rad :: Sphere -> Double
+rad  (Sphere (rad, _, _, _, _   )) = rad
+
+pos :: Sphere -> Vec
+pos  (Sphere (_  , p, _, _, _   )) = p
+
+emit :: Sphere -> Vec
+emit (Sphere (_  , _, e, _, _   )) = e
+
+col :: Sphere -> Vec
+col  (Sphere (_  , _, _, c, _   )) = c
+
+refl :: Sphere -> Refl
+refl (Sphere (_  , _, _, _, refl)) = refl
 
 intersect :: Sphere -> Ray -> Double
 intersect sp ray =
     let op  = (pos sp) - (org ray)
         b   = op `dot` (dir ray)
         det = b * b - (op `dot` op) + ((rad sp) ** 2)
-    in if det < 0.0 then inf
-                    else
-                        let sqdet = sqrt det
-                            t1    = b - sqdet
-                            t2    = b + sqdet
-                        in ansCheck t1 t2
-                              where ansCheck t1 t2
-                                        | t1 > eps  = t1
-                                        | t2 > eps  = t2
-                                        | otherwise = inf
+    in
+        if det < 0.0
+            then inf
+            else
+                let sqdet = sqrt det
+                    t1    = b - sqdet
+                    t2    = b + sqdet
+                in ansCheck t1 t2
+                      where ansCheck t1 t2
+                                | t1 > eps  = t1
+                                | t2 > eps  = t2
+                                | otherwise = inf
 
 -- Scene
-sph :: [Sphere]
+type Scene = [Sphere]
+sph :: Scene
 sph =  [ Sphere (1e5,  Vec ( 1e5+1,  40.8, 81.6),    Vec (0.0, 0.0, 0.0), Vec (0.75, 0.25, 0.25),  Diff) ] -- Left
     ++ [ Sphere (1e5,  Vec (-1e5+99, 40.8, 81.6),    Vec (0.0, 0.0, 0.0), Vec (0.25, 0.25, 0.75),  Diff) ] -- Right
     ++ [ Sphere (1e5,  Vec (50.0, 40.8,  1e5),       Vec (0.0, 0.0, 0.0), Vec (0.75, 0.75, 0.75),  Diff) ] -- Back
@@ -93,15 +120,11 @@ sph =  [ Sphere (1e5,  Vec ( 1e5+1,  40.8, 81.6),    Vec (0.0, 0.0, 0.0), Vec (0
 clamp :: Double -> Double
 clamp = (max 0.0) . (min 1.0)
 
+isectWithScene :: Scene -> Ray -> (Double, Int)
+isectWithScene scene ray = foldr1 (min) $ zip [ intersect sph ray | sph <- scene ] [0..]
 
-isectWithScene :: [Sphere] -> Ray -> (Int, Double)
-isectWithScene scene ray =
-    let n  = length scene
-        ds = map (\sp -> intersect sp ray) scene
-    in foldr1 minWithId $ zip [0..] ds
-           where minWithId p q
-                     | (snd p) < (snd q) = p
-                     | otherwise         = q
+nextDouble :: IO Double
+nextDouble = randomIO
 
 lambert :: Vec -> Double -> Double -> (Vec, Double)
 lambert n r1 r2 =
@@ -145,56 +168,60 @@ refract v n orn rr =
                          then (rdir, (pp / re))
                          else (tdir, ((1.0 - pp) / tr))
 
-radiance :: [Sphere] -> Ray -> Int -> IO Vec
+radiance :: Scene -> Ray -> Int -> IO Vec
 radiance scene ray depth = do
-    let (i, t) = (isectWithScene scene ray)
-    if t < inf
-        then (radiance' scene (scene !! i) ray t depth)
-        else return (Vec (0, 0, 0))
+    let (t, i) = (isectWithScene scene ray)
+    r0 <- nextDouble
+    r1 <- nextDouble
+    r2 <- nextDouble
+    if inf <= t
+        then return (Vec (0, 0, 0))
+        else (radiance' scene ray depth (scene !! i) t r0 r1 r2)
 
-radiance' :: [Sphere] -> Sphere -> Ray -> Double -> Int -> IO Vec
-radiance' scene obj ray t depth = do
+radiance' :: Scene -> Ray -> Int -> Sphere -> Double -> Double -> Double -> Double -> IO Vec
+radiance' scene ray depth obj t r0 r1 r2 = do
     let c = col obj
     let rlt = (max (x c) (max (y c) (z c)))
-    r0 <- randomIO :: IO Double
-    if depth >= 5
-        then
-            if r0 < rlt
-                then radiance'' scene obj ray t rlt depth
-                else return (emit obj)
-        else radiance'' scene obj ray t 1.0 depth
+    if depth < 5
+        then radiance'' scene ray depth obj t 1.0 r1 r2
+        else
+            if r0 >= rlt
+                then return $! (emit obj)
+                else radiance'' scene ray depth obj t rlt r1 r2
 
-radiance'' :: [Sphere] -> Sphere -> Ray -> Double -> Double -> Int -> IO Vec
-radiance'' scene obj ray t rlt depth = do
+radiance'' :: Scene -> Ray -> Int -> Sphere -> Double -> Double -> Double -> Double -> IO Vec
+radiance'' scene ray depth obj t rlt r1 r2 = do
     let f = (col obj)
     let d = (dir ray)
     let x = (org ray) + (d `mul` t)
     let n = norm $ x - (pos obj)
     let orn = if (d `dot` n) < 0.0  then n else (-n)
-    r1 <- randomIO :: IO Double
-    r2 <- randomIO :: IO Double
     let (ndir, pdf) = case (refl obj) of
             Diff -> (lambert orn r1 r2)
             Spec -> (reflect d orn)
             Refr -> (refract d n orn r1)
     nextRad <- (radiance scene (Ray (x, ndir)) (succ depth))
-    return $ (emit obj) + ((f * nextRad) `mul` (1.0 / (rlt * pdf)))
+    return $! ((emit obj) + ((f * nextRad) `mul` (1.0 / (rlt * pdf))))
 
 toByte :: Double -> W.Word8
 toByte x = truncate (((clamp x) ** (1.0 / 2.2)) * 255.0) :: W.Word8
 
-accumulateRadiance :: IO Vec -> Int -> Int -> IO Vec
-accumulateRadiance r d m
-    | d == m    = return (Vec (0.0, 0.0, 0.0))
-    | otherwise = (+) <$> (fmap (`mul` (1.0 / (fromIntegral m))) r) <*> (accumulateRadiance r (succ d) m)
+accumulateRadiance :: Scene -> Ray -> Int -> Int -> IO Vec
+accumulateRadiance scene ray d m
+    | d == m    = do
+        return (Vec (0.0, 0.0, 0.0))
+    | otherwise = do
+        r1 <- radiance scene ray 0
+        r2 <- (accumulateRadiance scene ray (succ d) m)
+        return $! ((r1 `mul` (1.0 / fromIntegral m)) + r2)
 
 main :: IO ()
 main = do
     args <- getArgs
     let argc = length args
-    let w   = if argc >= 1 then (read (args !! 0)) else 400
-    let h   = if argc >= 2 then (read (args !! 1)) else 300
-    let spp = if argc >= 3 then (read (args !! 2)) else 8
+    let w   = if argc >= 1 then (read (args !! 0)) else 400 :: Int
+    let h   = if argc >= 2 then (read (args !! 1)) else 300 :: Int
+    let spp = if argc >= 3 then (read (args !! 2)) else 4   :: Int
 
     startTime <- getCurrentTime
 
@@ -212,9 +239,9 @@ main = do
     let dirs = [ norm $ (dir cam) + (cy `mul` (y / dh  - 0.5)) + (cx `mul` (x / dw - 0.5)) | (y, x) <- liftM2 (,) [dh-1,dh-2..0] [0..dw-1] ]
     let rays = [ Ray ((org cam) + (d `mul` 140.0), (norm d)) | d <- dirs ]
 
-    pixels <- foldr (\e lst -> (:) <$> e <*> lst) (return []) $ [ (accumulateRadiance (radiance sph r 0) 0 spp) | r <- rays ]
+    pixels <- sequence [ (accumulateRadiance sph r 0 spp) | r <- rays ]
 
-    let pixelData = map toByte $ foldr (\col lst -> [(x col), (y col), (z col)] ++ lst) [] pixels
+    let pixelData = map toByte $! pixels `seq` (foldr (\col lst -> [(x col), (y col), (z col)] ++ lst) [] pixels)
     let pixelBytes = V.fromList pixelData :: V.Vector W.Word8
     let img = Image { imageHeight = h, imageWidth = w, imageData = pixelBytes } :: Image PixelRGB8
     writePng "image.png" img
